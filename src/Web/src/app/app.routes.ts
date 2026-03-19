0de7302cc91c7dcdd78447be48d520ac9231b236
David <dmoraroca@gmail.com>
2026-03-19 13:39:30 +0100

    002 => Template acabat

--- src/Web/src/app/app.routes.ts
+++ src/Web/src/app/app.routes.ts
@@ -5,5 +5,12 @@ export const routes: Routes = [
     path: '',
     loadComponent: () =>
       import('./features/home/pages/home-page.component').then((m) => m.HomePageComponent)
+  },
+  {
+    path: 'permissions',
+    loadComponent: () =>
+      import('./features/permissions/pages/permissions-page.component').then(
+        (m) => m.PermissionsPageComponent
+      )
   }
 ];
--- src/Web/src/app/core/layout/components/site-footer.component.ts
+++ src/Web/src/app/core/layout/components/site-footer.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-site-footer',
+  imports: [RouterLink],
   templateUrl: './site-footer.component.html',
   styleUrl: './site-footer.component.scss'
 })
